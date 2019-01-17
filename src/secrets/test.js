be9002811ec2b372066382246278b62ade2b321d
Viorel Rinja <viorelrinja@gmail.com>
2019-01-17 22:15:00 +0200

    update project packages

---
+++ src/secrets/test.js
@@ -0,0 +1,4 @@
+const secrets = {
+  accessKey = '56c5f78391ac7ae57ec1d4bd32f8f446808b4bec1f4d910a83af8e44b4eddde8',
+  secretKey = '1f384e9577801f9d249fb81b2160c386778450234ce6c73e4ee02cd2e2c53fed'
+}
