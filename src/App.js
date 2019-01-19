c51d4bd400269da1291bec65a20f582d279c92cb
Viorel Rinja <viorelrinja@gmail.com>
2019-01-19 13:00:40 +0200

    use link for fonts instead of @include; implement one-button under the hood oAuth login

--- src/App.js
+++ src/App.js
@@ -4,100 +4,103 @@ import React, { Component } from 'react'
 import {accessKey, secretKey} from './secrets.js'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      accessToken: '',
-      refreshToken: '',
-      tokenType: ''
-    }
-    this.oAuth = this.oAuth.bind(this)
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			accessToken: '',
+			refreshToken: '',
+			tokenType: ''
+		}
+		this.oAuth = this.oAuth.bind(this)
+	}
 
-  saveStateInStorage() {
-    localStorage.setItem('state', JSON.stringify(this.state))
-  }
+	saveStateInStorage() {
+		localStorage.setItem('state', JSON.stringify(this.state))
+	}
 
-  componentDidUpdate () {
-    this.saveStateInStorage.bind(this)()
-  }
+	componentDidUpdate () {
+	}
 
-  componentDidMount() {
-    const state = localStorage.getItem('state')
-    this.setState(JSON.parse(state))
-  }
+	componentDidMount() {
+		if (this.getResponseCode()) {
+			window.location = 'http://localhost:3000'
+		}
 
-  unloggedRequest() {
-    fetch('https://api.unsplash.com/photos/', {
-      headers: {
-        'Authorization': 'Client-ID ' + accessKey
-      }
-    })
-    .then((res) => res.json())
-  }
+		const state = localStorage.getItem('state')
+		if (state) {
+			this.setState(JSON.parse(state))
+		}
+	}
 
-  oAuth() {
-    // fetch('https://unsplash.com/oauth/authorize', {
-    //   headers: {
-    //     'client_id': this.state.accessKey,
-    //     'redirect_uri': 'htpp://localhost:3000',
-    //     'response_type': 'code',
-    //     'grant_type': 'public',
-    //     'Accept-Version': 'v1'
-    //   }
-    // })
-    // .then((res) => res)
-  }
+	unloggedRequest() {
+		fetch('https://api.unsplash.com/photos/', {
+			headers: {
+				'Authorization': 'Client-ID ' + accessKey
+			}
+		})
+		.then((res) => res.json())
+	}
 
-  redirect() {
-    let redirectURI = 'https://unsplash.com/oauth/authorize?client_id='+ accessKey +'&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&scope=public+read_user+write_user+read_photos+write_photos+write_likes+write_followers+read_collections+write_collections'
-    window.location = redirectURI
-  }
+	oAuth() {
+		// fetch('https://unsplash.com/oauth/authorize', {
+		//   headers: {
+		//     'client_id': this.state.accessKey,
+		//     'redirect_uri': 'htpp://localhost:3000',
+		//     'response_type': 'code',
+		//     'grant_type': 'public',
+		//     'Accept-Version': 'v1'
+		//   }
+		// })
+		// .then((res) => res)
+	}
 
-  getResponseCode() {
-    const url = new URL(window.location.href)
-    return url.searchParams.get('code')
-  }
+	redirect() {
+		let redirectURI = 'https://unsplash.com/oauth/authorize?client_id='+ accessKey +'&redirect_uri=http://localhost:3000&response_type=code&scope=public+read_user+write_user+read_photos+write_photos+write_likes+write_followers+read_collections+write_collections'
+		window.location = redirectURI
+	}
 
-  getToken() {
-    fetch('https://unsplash.com/oauth/token', {
-      method: 'POST',
-      headers: {
-        'Accept-Version': 'v1',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        'client_id': accessKey,
-        'client_secret': secretKey,
-        'redirect_uri': 'http://localhost:3000',
-        'code': this.getResponseCode(),
-        'grant_type': 'authorization_code'
-      })
-    })
-    .then(res => res.json())
-    .then(
-      res => this.setState((state = this.state) => {
-        state.accessToken = res.access_token
-        state.refreshToken = res.refresh_token
-        state.tokenType = res.token_type
-        return state
-      })
-    )
-  }
+	getResponseCode() {
+		const url = new URL(window.location.href)
+		return url.searchParams.get('code')
+	}
 
-  debug () {
-    console.log(this.state)
-    console.log(JSON.parse(localStorage.getItem('state')))
-  }
+	getToken() {
+		if (this.getResponseCode()) {
+			return false;
+		}
 
-  render () {
-    return (
-      <div>
-        
-         
-      </div>
-    )
-  }
+		fetch('https://unsplash.com/oauth/token', {
+			method: 'POST',
+			headers: {
+				'Accept-Version': 'v1',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				'client_id': accessKey,
+				'client_secret': secretKey,
+				'redirect_uri': 'http://localhost:3000',
+				'code': this.getResponseCode(),
+				'grant_type': 'authorization_code'
+			})
+		})
+		.then(res => res.json())
+		.then(res => res)
+		.then(res => localStorage.setItem('state', JSON.stringify(res)))
+	}
+
+	debug () {
+		console.log(this.state)
+		console.log(JSON.parse(localStorage.getItem('state')))
+	}
+
+	render () {
+		return (
+			<div>
+				<button onClick={this.redirect.bind(this)}>Log in</button>
+				<button onClick={this.debug.bind(this)}>Debug</button>
+			</div>
+		)
+	}
 }
 
 export default App;
